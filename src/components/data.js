4bbc1a6b1f3b2c1f902def5b8fb9730212d8203c
gabrielLapaOriginal <gabriel.lapa2001@yahoo.com.br>
2024-11-13 10:54:50 -0300

    Voltando a versao anterior

--- src/components/data.js
+++ src/components/data.js
@@ -1,42 +1,42 @@
 export const data = [
   {
     title: "O cuidado em saúde mental na atualidade",
-    image: "src/assets/Foto1.png",
+    image: "/assets/Foto1.png",
     description: "O objetivo desse trabalho foi identificar as características sócio-demográficas dos cuidadores de pessoas que tiveram alta hospitalar de internação psiquiátrica...",
     type: "modal", 
     link: "https://www.scielo.br/j/reeusp/a/QfTCHCJQHLYQBZ7wC8wZ9sK/?format=pdf&lang=pt"
   },
   {
     title: "A Saúde Mental Global, a Depressão, a Ansiedade e os Comportamentos de Risco...",
-    image: "src/assets/Foto2.png",
+    image: "/assets/Foto2.png",
     description: "A Saúde Mental Global, a Depressão, a Ansiedade e os Comportamentos de Risco nos Estudantes do Ensino Superior: Estudo de Prevalência e Correlação...",
     type: "modal", 
     link: "https://www.proquest.com/openview/0e3e9c35d5e4f422b9224af5adb5b238/1?pq-origsite=gscholar&cbl=2026366&diss=y"
   },
   {
     title: "Capacitação de saúde mental para professores do ensino fundamental e seu impacto...",
-    image: "src/assets/Foto3.png",
+    image: "/assets/Foto3.png",
     description: "Crianças afetadas por transtornos mentais são, em grande parte, não identificadas nem tratadas em todo o mundo. O sistema educacional está intimamente relacionado com o campo da saúde mental na infância...",
     type: "modal", 
     link: "https://www.teses.usp.br/teses/disponiveis/5/5142/tde-04022014-113920/en.php"
   },
   {
     title: "Ajuda para você ou seu amigo clique no código QR",
-    image: "src/assets/Foto4.png",
+    image: "/assets/Foto4.png",
     description: "Você será redirecionado para o contato do nosso psicólogo institucional e poderá falar com ele de forma anonima se preferir (trotes não serão permitidos)",
     type: "page", 
     route: "/Ajuda"
   },
   {
     title: "Lista de workshops sobre saúde mental, clique aqui se quiser entender mais",
-    image: "src/assets/Foto5.png",
+    image: "/assets/Foto5.png",
     description: "Você será redirecionado para nossa pagina de cadastro com as informações sobre eventos e workshops sobre saúde mental.",
     type: "page", 
     route: "/Workshops"
   },
   {
     title: "Como lidar melhor com meu diagnostico de doença relacionada a saúde mental ?",
-    image: "src/assets/Foto6.png",
+    image: "/assets/Foto6.png",
     description: "Se você foi diagnosticado recentemente ou tem um amigo ou colega que foi diagnosticado podemos disponibilizar textos para que você tenha um norte nessa jornada.",
     type: "page", 
     route: "/Diagnostico"
